7ec452fec292dea3763a92a77ded7b4ec7cd60e4
Elliott Kember <elliott.kember@gmail.com>
2020-08-27 09:13:34 -0700

    Export default new

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
-"use strict";
+// "use strict";
 
-const packageName = require("../package.json").name;
-const cosmiconfig = require("cosmiconfig");
-const log = require("loglevel");
+// const packageName = require("../package.json").name;
+// const cosmiconfig = require("cosmiconfig");
+// const log = require("loglevel");
 
-log.setLevel(process.env.LOG_LEVEL || "info");
+// log.setLevel(process.env.LOG_LEVEL || "info");
 
-module.exports = async function init(bundler) {
-  const explorer = cosmiconfig(packageName);
-  let config;
+// module.exports = async function init(bundler) {
+//   const explorer = cosmiconfig(packageName);
+//   let config;
 
-  try {
-    const result = await explorer.search();
-    config = result.config;
-  } catch (e) {
-    log.trace(e);
-  }
+//   try {
+//     const result = await explorer.search();
+//     config = result.config;
+//   } catch (e) {
+//     log.trace(e);
+//   }
 
-  const extensions = (config && config.extension) || ["graphql"];
+//   const extensions = (config && config.extension) || ["graphql"];
 
-  for (const ext of extensions) {
-    bundler.addAssetType(ext, require.resolve("./transformer"));
-  }
-};
+//   for (const ext of extensions) {
+//     bundler.addAssetType(ext, require.resolve("./transformer"));
+//   }
+// };
+
+
+import {Transformer} from "@parcel/plugin";
+import gql from "graphql";
+
+export default new Transformer({
+	async transform({asset}) {
+		let code = await asset.getCode();
+		let result = gql(code);
+		asset.setCode(`module.exports = ${JSON.stringify(result)};`);
+		return asset;
+	}
+}
